81629e46d3cd3456f56d084118b7872ff0816b40
Mahesh Sawaiker <maheshsa@us.ibm.com>
2017-03-27 18:01:26 +0530

    Made modifications to show dynamic headers

--- app/assets/javascripts/Tpcds.jsx
+++ app/assets/javascripts/Tpcds.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import axios from 'axios';
-
+import flatten from 'lodash/flatten';
+import uniq from 'lodash/uniq';
 
    function formatLoads(workloads,row){
+       //console.log(row)
         return <span>{workloads.map((workload) => <div key={workload.name}> {workload.name}
             </div> )}</span>
         }
+   function shortDate(date,row){
+       return new Date(date).toISOString().slice(0, 10);
+       }
 
 class TPCDS extends React.Component{
 	constructor(props){
 		super(props)
 		
 		this.state = {
-			bm_data : []
+			bm_data : [],
+            load_names: []
 		};
 	}
 	
@@ -20,34 +26,33 @@ class TPCDS extends React.Component{
 		axios.get("/api/t_benchmarks",
 				{headers: {'Content-Type': 'application/json'}})
 		.then(res=>{
-			this.setState({bm_data: res.data});
+            let all_wl = res.data.map(a => a.workloads.map(wl => wl.name))
+            var load_names = uniq(flatten(all_wl))
+            console.log(load_names)
+			
+            let mod_data  = res.data.map(row => {
+                for (var i in load_names)
+                { 
+                    //console.log(load_names);
+                    row[load_names[i]]="Hey there: "+load_names[i];
+                }
+                return row; 
+             });
+            console.log(mod_data)
+            this.setState({bm_data: mod_data});
+            this.setState({load_names: load_names})
 		});
 	}
 	
 	render(){
     
-        let columns = [
-                    {name: 'name'},
-                      { name: 'date' },
-                      {name: 'git_url'},
-                      {name:'workloads'}
-                ]
-
-        
-        let benchmarks =[
-            {name:"TPCDS",
-            date:new Date()
-            }
-            ]
-    
-        
 		return(
            <div>
-                <BootstrapTable data={this.state.bm_data}  keyField='date'>
+                <BootstrapTable data={this.state.bm_data}  keyField='date' striped>
                     <TableHeaderColumn dataField='name'>Benchmark Name</TableHeaderColumn>
-                    <TableHeaderColumn dataField='date'>Date</TableHeaderColumn>
+                    <TableHeaderColumn dataField='date' dataFormat={ shortDate }>Date</TableHeaderColumn>
                     <TableHeaderColumn dataField='git_url'>repo url</TableHeaderColumn>
-                    <TableHeaderColumn  dataField='workloads' dataFormat={ formatLoads }>Benchmarks</TableHeaderColumn>
+                    {this.state.load_names.map(name => <TableHeaderColumn  dataField={name} >{name}</TableHeaderColumn>)}
                 </BootstrapTable>
           </div>
             );
