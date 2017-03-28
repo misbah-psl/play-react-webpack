702b94042cdd5399ed1cd135c8015dc7d0f51897
Mahesh Sawaiker <maheshsa@us.ibm.com>
2017-03-28 14:15:18 +0530

    Data display complete 1) Bootstrap table with spanning columns 2) Pulling out metric data to outermost level

--- app/assets/javascripts/Tpcds.jsx
+++ app/assets/javascripts/Tpcds.jsx
@@ -22,22 +22,23 @@ class TPCDS extends React.Component{
 		};
 	}
 	
+     getBenchMarks(){
+        
+        return axios.get("/api/t_benchmarks",
+                {headers: {'Content-Type': 'application/json'}});
+        }
+    
 	componentDidMount(){
-		axios.get("/api/t_benchmarks",
-				{headers: {'Content-Type': 'application/json'}})
-		.then(res=>{
+		
+		this.getBenchMarks().then(res=>{
             let all_wl = res.data.map(a => a.workloads.map(wl => wl.name))
-            var load_names = uniq(flatten(all_wl))
+            let load_names = uniq(flatten(all_wl))
             console.log(load_names)
 			
-            let mod_data  = res.data.map(row => {
-                for (var i in load_names)
-                { 
-                    //console.log(load_names);
-                    row[load_names[i]]="Hey there: "+load_names[i];
-                }
-                return row; 
-             });
+            let mod_data  = res.data.map(row =>{ row.workloads.map(workload => workload.metrics.map(metric => { row[metric.name+workload.name] = metric.value}))
+            return row
+            });
+            console.log("first data:")
             console.log(mod_data)
             this.setState({bm_data: mod_data});
             this.setState({load_names: load_names})
@@ -49,10 +50,17 @@ class TPCDS extends React.Component{
 		return(
            <div>
                 <BootstrapTable data={this.state.bm_data}  keyField='date' striped>
-                    <TableHeaderColumn dataField='name'>Benchmark Name</TableHeaderColumn>
-                    <TableHeaderColumn dataField='date' dataFormat={ shortDate }>Date</TableHeaderColumn>
-                    <TableHeaderColumn dataField='git_url'>repo url</TableHeaderColumn>
-                    {this.state.load_names.map(name => <TableHeaderColumn  dataField={name} >{name}</TableHeaderColumn>)}
+                    <TableHeaderColumn tdStyle={ { 'width': '150px' } } thStyle={ { 'width': '150px' } }  row='0' rowSpan='2' dataField='name'>Benchmark Name</TableHeaderColumn>
+                    <TableHeaderColumn tdStyle={ { 'width': '150px' } } thStyle={ { 'width': '150px' } } row='0' rowSpan='2' dataField='date' >Date</TableHeaderColumn>
+                    <TableHeaderColumn tdStyle={ { 'width': '150px' } } thStyle={ { 'width': '150px' } } row='0' rowSpan='2' dataField='git_url' > repo url</TableHeaderColumn>
+
+              {this.state.load_names.map(
+                       function(name) { return [<TableHeaderColumn  row='0' colSpan='4' tdStyle={ { 'width': '400px' } } thStyle={ { 'width': '400px' } } >{name}</TableHeaderColumn>,
+                            <TableHeaderColumn  tdStyle={ { 'width': '100px' } } thStyle={ { 'width': '100px' } }  dataField={"minTimeMs"+name} row='1'>min-time</TableHeaderColumn>,
+                            <TableHeaderColumn tdStyle={ { 'width': '100px' } } thStyle={ { 'width': '100px' } } dataField={"maxTimeMs"+name} row='1'>max-time</TableHeaderColumn>,
+                            <TableHeaderColumn tdStyle={ { 'width': '100px' } } thStyle={ { 'width': '100px' } } dataField={"avgTimeMs"+name} row='1'>avg-time</TableHeaderColumn>,
+                            <TableHeaderColumn tdStyle={ { 'width': '100px' } } thStyle={ { 'width': '100px' } } dataField={"stdDev"+name} row='1'>std-dev</TableHeaderColumn> ]
+                        })}
                 </BootstrapTable>
           </div>
             );
