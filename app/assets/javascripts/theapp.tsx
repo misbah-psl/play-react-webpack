d84135387d95ef7692b58a742117b608a293a8a7
Mahesh Sawaiker <maheshsa@us.ibm.com>
2017-04-05 16:29:08 +0530

    Testing another graph

--- app/assets/javascripts/theapp.tsx
+++ app/assets/javascripts/theapp.tsx
@@ -3,6 +3,7 @@ import * as ReactDOM from 'react-dom';
 import {Greeter} from './components/Greeter';
 import {TPCDS} from './components/Tpcds';
 import {Graph} from './components/Graph';
+import {Graph2} from './components/Graph2';
 
 import {
   BrowserRouter,
@@ -29,14 +30,16 @@ class TheApp extends React.Component<any,any>{
         <div className="nav">
         <ul>
             <li> <Link to="/app">Home </Link> </li>
-        <li> <Link to="/app/about">About </Link> </li>
+        <li> <Link to="/app/rawdata">Raw data</Link> </li>
         <li> <Link to="/app/graph">Graph</Link> </li>
+        <li> <Link to="/app/graph2">Graph2</Link> </li>
         <li> <Link to="/app/spinner">Spinner</Link> </li>
         </ul>
                 <Route exact path="/app" component={() => (<Greeter name="User"/>)}/>
-                <Route path="/app/about" component={TPCDS}/>
+                <Route path="/app/rawdata" component={TPCDS}/>
                 <Route path="/app/spinner" component={Loader}/>
                 <Route path="/app/graph" component={Graph}/>
+                <Route path="/app/graph2" component={Graph2}/>
         </div>
         </BrowserRouter>);
         }
